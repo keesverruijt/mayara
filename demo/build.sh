07fcf6ca3d691921f6ed08738f29be22e1c5bf89
Kees Verruijt <kees@verruijt.net>
2024-09-24 13:57:28 +0200

    Update demo

--- demo/build.sh
+++ demo/build.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
 
+set -euo pipefail
+
 docker buildx build --no-cache -t keesverruijt/mayara:latest .
+
+echo "Now run the image locally with:
+
+docker run --name mayara-demo -p 3000-3001:3000-3001 keesverruijt/mayara:latest
+"
 
