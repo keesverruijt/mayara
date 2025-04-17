faa2d6d502962f5738b07985502792703870e3fb
Kees Verruijt <kees@verruijt.net>
2025-04-17 10:52:08 +0200

    Issue #6: do not use ARPA in demo for now

--- demo/start.sh
+++ demo/start.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-./mayara -i lo -p 3001 --replay --brand navico &
+./mayara -i lo -p 3001 --replay --brand navico --targets trails &
 signalk-server/bin/signalk-server -c signalk/ &
 tcpreplay -q -T select -l 0 -i lo /app/halo_and_0183.pcap
