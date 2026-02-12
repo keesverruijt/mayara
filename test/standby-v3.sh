94f975e360d6fb19f3c23c97dfb00f5dda4733f8
Kees Verruijt <kees@verruijt.net>
2026-02-12 13:51:41 -0400

    Add test to standby all radars

---
+++ test/standby-v3.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -euo pipefail
+
+HOST=${1:-10.56.0.1}
+
+BASE_URL="http://${HOST}:6502"
+V3="${BASE_URL}/v3/api"
+
+radars=$(curl -s "${V3}/radars" | jq -r '.radars | keys[]')
+
+for radar in ${radars}
+do
+  echo "Standby radar ${radar}"
+  curl -s --json '{"value":"1"}' "${V3}/radars/${radar}/controls/power"
+  echo ""
+done
+
+
+
